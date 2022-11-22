55aaf791fccea1b30e96017750e3e2508d2aa013
Tatiana Ivanova <tat.mi.iva@gmail.com>
2022-11-22 12:56:09 +0300

    refactor: change class names for popups and their child elements

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,13 @@ const userNameElement = profileInfoElement.querySelector('.profile__name');
 const userJobElement = profileInfoElement.querySelector('.profile__desc');
 const profileEditBtn = profileInfoElement.querySelector('.profile__edit-btn');
 
+const profilePopupElement = document.querySelector('.popup_content_edit-profile');
+const closePopupBtn = profilePopupElement.querySelector('.popup__close-btn');
+const editProfileFormElement =  profilePopupElement.querySelector('.form_type_edit-profile');
+const inputProfileNameElement = editProfileFormElement.querySelector('.form__input_content_name');
+const inputJobElement = editProfileFormElement.querySelector('.form__input_content_job');
+
+
 function getProfileInfo() {
   const profileInfo = {};
   profileInfo.userName = userNameElement.textContent;
@@ -11,30 +18,23 @@ function getProfileInfo() {
 }
 
 function updateProfileInfo() {
-  const name = inputNameElement.value;
+  const name = inputProfileNameElement.value;
   const job = inputJobElement.value;
 
   userNameElement.textContent = name;
   userJobElement.textContent = job;
 }
 
-
-const popupElement = document.querySelector('.popup');
-const closePopupBtn = popupElement.querySelector('.popup__close-btn');
-const editFormElement =  popupElement.querySelector('.edit-form');
-const inputNameElement = editFormElement.querySelector('.edit-form__input_content_name');
-const inputJobElement = editFormElement.querySelector('.edit-form__input_content_job');
-
 function openPopup() {
   const profileInfo = getProfileInfo();
-  inputNameElement.value = profileInfo.userName;
+  inputProfileNameElement.value = profileInfo.userName;
   inputJobElement.value = profileInfo.userJob;
 
-  popupElement.classList.add('popup_open');
+  profilePopupElement.classList.add('popup_open');
 }
 
 function closePopup() {
-  popupElement.classList.remove('popup_open');
+  profilePopupElement.classList.remove('popup_open');
 }
 
 function formSubmitHandler(evt) {
@@ -95,4 +95,4 @@ cardsData.forEach(card => createCard(card));
 
 profileEditBtn.addEventListener('click', openPopup);
 closePopupBtn.addEventListener('click', closePopup);
-editFormElement.addEventListener('submit', formSubmitHandler);
+editProfileFormElement.addEventListener('submit', formSubmitHandler);
