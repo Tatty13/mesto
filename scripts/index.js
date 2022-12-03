ddb77626aa68c39f83b1e366b97f8d1e4f79f988
Tatiana Ivanova <tat.mi.iva@gmail.com>
2022-12-03 16:59:21 +0300

    refactor: move cardsData array to the separate file, use js moduules

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+import cardsData from './cardsData.js';
+
 const profileInfoElement = document.querySelector('.profile__info');
 const userNameElement = profileInfoElement.querySelector('.profile__name');
 const userJobElement = profileInfoElement.querySelector('.profile__desc');
@@ -127,40 +129,11 @@ function addCardFormSubmitHandler(evt) {
   closePopup(evt);
 }
 
-
 /* ----------- default cards ----------- */
-
-const cardsData = [
-  {
-    name: 'Шлиссельбург',
-    link: './images/place-oreshek.jpg'
-  },
-  {
-    name: 'Рускеала, Карелия',
-    link: './images/place-ruskeala.jpg'
-  },
-  {
-    name: 'Ленинградская область',
-    link: './images/place-yastrebinoe.jpg'
-  },
-  {
-    name: 'Санкт-Петербур',
-    link: './images/place-spb.jpg'
-  },
-  {
-    name: 'Великие Луки',
-    link: './images/place-velikie-luki.jpg'
-  },
-  {
-    name: 'Вологодская область',
-    link: './images/place-vologodskaya.jpg'
-  },
-]
 
 cardsData.forEach(card => createCard(card));
 
 /* ----------- ----- ----------- */
-
 
 profileEditBtn.addEventListener('click', () => openPopup(profilePopupElement));
 addCardBtn.addEventListener('click', () => openPopup(addCardPopupElement));
